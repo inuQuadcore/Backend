94345e5ebd45428968dd1974750027c7ebf1bc06
Oh-jj <fvjung01@gmail.com>
2025-11-05 16:02:26 +0900

    fix ci unit test 에러

--- src/test/java/com/example/everybuddy/EverybuddyApplicationTests.java
+++ src/test/java/com/example/everybuddy/EverybuddyApplicationTests.java
@@ -1,11 +1,8 @@
 package com.example.everybuddy;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+
 class EverybuddyApplicationTests {
 
 	@Test

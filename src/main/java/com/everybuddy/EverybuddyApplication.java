c57154ecf4705eae39a29d7aadfcddb8849109ad
Jun-77 <ghkdqhdmswns@naver.com>
2026-01-05 14:35:19 +0900

    feat: 메시지 hard delete 스케줄러 작성

--- src/main/java/com/everybuddy/EverybuddyApplication.java
+++ src/main/java/com/everybuddy/EverybuddyApplication.java
@@ -3,14 +3,14 @@ package com.everybuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class EverybuddyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EverybuddyApplication.class, args);
 	}
-
 }
 

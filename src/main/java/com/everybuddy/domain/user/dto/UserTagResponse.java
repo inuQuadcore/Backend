5d4e2f66402b9c039d02046f1a1141c4d29e0e69
Jun-77 <ghkdqhdmswns@naver.com>
2026-03-07 10:52:49 +0900

    refactor: DTO 리팩토링

--- src/main/java/com/everybuddy/domain/user/dto/UserTagResponse.java
+++ src/main/java/com/everybuddy/domain/user/dto/UserTagResponse.java
@@ -2,21 +2,29 @@ package com.everybuddy.domain.user.dto;
 
 import com.everybuddy.domain.user.entity.UserTag;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@Schema(description = "유저 태그 응답")
 public class UserTagResponse {
 
-    @Schema(description = "태그")
+    @Schema(description = "태그", example = "SPORTS")
     private String tag;
+
+    @Schema(description = "태그 카테고리", example = "HOBBY")
     private String category;
 
+    @Builder
+    private UserTagResponse(String tag, String category) {
+        this.tag = tag;
+        this.category = category;
+    }
+
     public static UserTagResponse from(UserTag userTag) {
-        return new UserTagResponse(
-                userTag.getTag().name(),
-                userTag.getTag().getCategory().name()
-        );
+        return UserTagResponse.builder()
+                .tag(userTag.getTag().name())
+                .category(userTag.getTag().getCategory().name())
+                .build();
     }
 }

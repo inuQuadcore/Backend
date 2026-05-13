045a7bd93ebd1fe469ce772f926d77bb9854e283
Jun-77 <ghkdqhdmswns@naver.com>
2026-05-13 13:23:36 +0900

    fix: 재로그인 시 RefreshToken unique 제약 위반

--- src/main/java/com/everybuddy/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/everybuddy/domain/auth/repository/RefreshTokenRepository.java
@@ -10,6 +10,8 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByToken(String token);
 
+    Optional<RefreshToken> findByUser(User user);
+
     void deleteByToken(String token);
 
     void deleteByUser(User user);
